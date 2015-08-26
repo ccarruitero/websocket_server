0128ab04d815b1398ccbacac1c034f58c85e3dee
ccarruitero <cesar@mozilla.pe>
2015-08-25 23:49:41 -0500

    only use ext gem to handle protocol

--- Gemfile
+++ Gemfile
@@ -2,5 +2,4 @@ source 'https://rubygems.org'
 
 ruby '2.2.2'
 
-gem 'faye-websocket'
-gem 'puma'
+gem 'websocket'
--- server.rb
+++ server.rb
@@ -1,31 +1,78 @@
-require 'faye/websocket'
+require 'websocket'
+require 'socket'
 
-@clients = []
-KEEPALIVE_TIME = ENV['KEEP_ALIVE']
+module WebSocketExample
+  class Server
+    attr_accessor :clients
 
-App = lambda do |env|
-  ws = Faye::WebSocket.new(env)
+    def initialize host='localhost', port='2000'
+      @tcp_server = TCPServer.new(host, port)
+      @clients = []
+    end
 
-  ws.on :open do |event|
-    p [:open, ws.object_id]
-    @clients << ws
-  end
+    def accept
+      socket = @tcp_server.accept
+      handshake = WebSocket::Handshake::Server.new
 
-  ws.on :message do |event|
-    p [:message, event.data]
-    @clients.each do |cli|
-      cli.send(event.data)
+      while line = socket.gets
+        handshake << line
+        break if handshake.finished?
+      end
+
+      client = Client.new(socket, handshake)
+      close(client) unless handshake.valid?
+      @clients << client
+      client.socket.write handshake
+      client
+    end
+
+    def close client
+      @clients.delete client
+      client.socket.close
+    end
+
+    def read message, client
+      # parse message from socket and send to others clients
+      client.frame << message
+      msg = client.frame.next
+
+      if msg.type == :close
+        close client
+      else
+        send(msg, client)
+      end
+    end
+
+    def send msg, owner
+      # send message to clients except message owner
+      @clients.each do |client|
+        if client.socket != owner.socket
+          # TODO: allow more types
+          frame = WebSocket::Frame::Outgoing::Server.new(:version => client.handshake.version, :data => msg, :type => :text)
+          client.socket.write frame
+        end
+      end
     end
   end
 
-  ws.on :close do |event|
-    p [:close, event.code, event.reason]
-    @clients.delete(ws)
-    ws = nil
+  class Client
+    # To store socket data that would be used later
+    attr_accessor :socket, :handshake, :frame
+
+    def initialize socket, handshake
+      @socket = socket
+      @handshake = handshake
+      @frame = WebSocket::Frame::Incoming::Server.new(:version => handshake.version)
+    end
   end
+end
 
-  # Return async Rack response
-  puts 'websocket server initiated'
-  ws.rack_response
+server = WebSocketExample::Server.new
 
+loop do
+  Thread.new(server.accept) do |client|
+    while (d = client.socket.recvfrom(2000))
+      server.read(d[0], client)
+    end
+  end
 end
